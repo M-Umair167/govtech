41e6e279ad7d6b56164975130659866c49377144
Muhammad Umair <umairbwp202@gmail.com>
2026-02-09 12:19:42 +0500

    requirement file is moved to root and vercel link in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,6 +27,7 @@ origins = [
     "http://127.0.0.1:3001",
     "http://192.168.1.10:5366",
     "http://192.168.1.10:3000",
+    "https://your-app.vercel.app",
     "https://1pczgwxx-5366.inc1.devtunnels.ms",
     "https://1pczgwxx-5366.inc1.devtunnels.ms/",
 ]
